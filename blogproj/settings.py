afedce121f303afaa6bb002c710c0eca1f49fe5a
Muhammad Fahad Ali <fadiuoh2003@gmail.com>
2026-07-06 18:30:58 +0500

    Fixing css correctly

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -138,3 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+STATICFILES_DIRS = [BASE_DIR / 'posts/static']
